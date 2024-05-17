26d60adc4fcc9f8dd329a86b2ad95adeae3d2caa
dot-mike <586280+dot-mike@users.noreply.github.com>
2024-05-17 22:52:33 +0200

    Add metal script for power mgmt

---
+++ scripts/cluster-power.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <on|off>"
+    exit 1
+fi
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+ansible-playbook "$SCRIPT_DIR/../metal/host-power.yml" --extra-vars "state=$1"
