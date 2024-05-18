67dfa9b4e7706e8b237fa0c0c5c38798c40a0695
dot-mike <586280+dot-mike@users.noreply.github.com>
2024-05-18 22:00:20 +0200

    Update scripts for ansible-vault encryption

--- scripts/vault-decrypt.sh
+++ scripts/vault-decrypt.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+#!/bin/sh
+
 SCRIPT_DIR=$(realpath $(dirname $0))
-ansible-vault decrypt "${SCRIPT_DIR}/../metal/inventory/group_vars/proxmox/vault.yml"
-ansible-vault decrypt "${SCRIPT_DIR}/../metal/inventory/group_vars/all/vault.yml"
+
+FILES=(
+    "${SCRIPT_DIR}/../metal/inventory/group_vars/proxmox/vault.yml"
+    "${SCRIPT_DIR}/../metal/inventory/group_vars/all/vault.yml"
+    "${SCRIPT_DIR}/../.env"
+)
+
+for FILE in "${FILES[@]}"; do
+    if [ ! -f "$FILE" ]; then
+        echo "File not found: $FILE"
+        exit 1
+    fi
+
+    OWNER=$(stat -c %u "$FILE")
+    PERMS=$(stat -c %a "$FILE")
+
+    ansible-vault decrypt "$FILE"
+
+    chown $OWNER "$FILE"
+    chmod $PERMS "$FILE"
+done
--- scripts/vault-encrypt.sh
+++ scripts/vault-encrypt.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(realpath $(dirname $0))
-ansible-vault encrypt "${SCRIPT_DIR}/../metal/inventory/group_vars/proxmox/vault.yml"
-ansible-vault encrypt "${SCRIPT_DIR}/../metal/inventory/group_vars/all/vault.yml"
+
+FILES=(
+    "${SCRIPT_DIR}/../metal/inventory/group_vars/proxmox/vault.yml"
+    "${SCRIPT_DIR}/../metal/inventory/group_vars/all/vault.yml"
+    "${SCRIPT_DIR}/../.env"
+)
+
+for FILE in "${FILES[@]}"; do
+    if [ ! -f "$FILE" ]; then
+        echo "File not found: $FILE"
+        exit 1
+    fi
+
+    OWNER=$(stat -c %u "$FILE")
+    PERMS=$(stat -c %a "$FILE")
+
+    ansible-vault encrypt "$FILE"
+
+    chown $OWNER "$FILE"
+    chmod $PERMS "$FILE"
+done
