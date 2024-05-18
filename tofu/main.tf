1a2d6ddf9d5bf458bdc360b843bfcab7485e1b04
dot-mike <586280+dot-mike@users.noreply.github.com>
2024-05-18 22:01:17 +0200

    tofu add in persistent state in s3

--- tofu/main.tf
+++ tofu/main.tf
@@ -2,7 +2,28 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.57.0"
+      version = "~> 0.57.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50"
+    }
+  }
+
+  encryption {
+    method "aes_gcm" "default" {
+      keys = key_provider.pbkdf2.my_passphrase
+    }
+
+    state {
+      method = method.aes_gcm.default
+      enforced = true
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-homeops"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-central-1"
   }
 }
